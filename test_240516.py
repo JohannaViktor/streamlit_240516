c5e177676ac274819bbbf5c2c23adce2696d1fb7
Viktor <Johanna.Viktor@bvg.de>
2024-05-16 11:00:59 +0200

    first push

---
+++ test_240516.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.write("Hello World!")
+st.write("Hello to you,too!")
+
+st.header("Streamlit Task")
+st.subheader("Streamlit Subtask")
+st.write("blabla")
+st.video("https://www.youtube.com/watch?v=GACvN11rpBo")
